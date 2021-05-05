19d48556c7558d3d59767ce59db1adee52059479
Bryan Yu <70780073+bryanyu1@users.noreply.github.com>
2021-05-05 09:39:32 -0700

    Update MySimpson.m

--- MySimpson.m
+++ MySimpson.m
@@ -4,7 +4,7 @@ function [x] = MySimpson(u,a,b,N)
     for i = 1:N
         y(i + 1) = a + i * h;
     end
-    sum_1 = 0; 
+    sum_1 = 0;
     sum_2 = 0;
     for i = 1:N 
         x_i = (y(i + 1) + y (i)) / 2;
